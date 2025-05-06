abfd478c81d84a4b1eb31c6af059c1aac762b9b8
joemiddie13 <joemiddie13@gmail.com>
2025-05-06 15:30:13 -0700

    Added an UpSplash image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -63,8 +63,8 @@ function HomePage() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="/api/placeholder/1200/800"
-            alt="Policy discussion"
+            src="/images/meeting-room.avif"
+            alt="Policy discussion forum"
           />
         </div>
       </div>
